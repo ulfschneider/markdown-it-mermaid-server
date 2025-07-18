6877c51a28ed299e678eb6a917396975b5dee7d6
ulfschneider <mail@ulfschneider.io>
2025-07-18 20:20:18 +0200

    Use a different approach by embedding the chart as an svg tag

--- index.mjs
+++ index.mjs
@@ -3,20 +3,14 @@ import path from "node:path";
 import { execSync } from "node:child_process";
 import chalk from "chalk";
 import pkg from "./package.json" with { type: "json" };
-import hashSum from "hash-sum";
 
 let settings = {
   workingFolder: "mermaidTmp",
-  outputFolder: "mermaid",
-  renderPath: "/mermaid/",
-  useDataUri: false,
+  clearWorkingFolder: false,
   backgroundColor: "white",
   themeCSS: "",
-  imageAttributes: "",
   mermaidConfig: {},
   puppeteerConfig: {},
-  clearWorkingFolder: false,
-  clearOutputFolder: false,
   verbose: false,
 };
 
@@ -30,18 +24,6 @@ function printSettings() {
   }
 }
 
-function makeUrlPath(chartFileName) {
-  const path = settings.renderPath
-    .split("/")
-    .filter((part) => part != "")
-    .join("/");
-  return "/" + path + "/" + chartFileName;
-}
-
-function makeOutputFilePath(fileName) {
-  return path.join(settings.outputFolder, fileName);
-}
-
 function makeWorkingFilePath(fileName) {
   return path.join(settings.workingFolder, fileName);
 }
@@ -53,7 +35,7 @@ function makeFolderPath(folderPath) {
 function initialize(options) {
   settings = Object.assign(settings, options);
   settings.workingFolder = makeFolderPath(settings.workingFolder);
-  settings.outputFolder = makeFolderPath(settings.outputFolder);
+
   printSettings();
 
   if (fs.existsSync(settings.workingFolder) && settings.clearWorkingFolder) {
@@ -70,13 +52,10 @@ function initialize(options) {
     makeWorkingFilePath("puppeteerConfig.json"),
     JSON.stringify(settings.puppeteerConfig),
   );
-  fs.writeFileSync(makeWorkingFilePath("theme.css"), settings.themeCSS);
-  if (fs.existsSync(settings.outputFolder) && settings.clearOutputFolder) {
-    fs.rmSync(settings.outputFolder, { recursive: true, force: true });
-  }
-  if (!settings.useDataUri && !fs.existsSync(settings.outputFolder)) {
-    fs.mkdirSync(settings.outputFolder);
-  }
+  fs.writeFileSync(
+    makeWorkingFilePath("theme.css"),
+    settings.themeCSS ? settings.themeCSS.replaceAll("\n", "") : "", //no newlines because of markdown processing
+  );
 }
 
 function extractChartTitle(chartDefinition) {
@@ -93,49 +72,31 @@ function extractChartTitle(chartDefinition) {
   }
 }
 
-function getChartFileLocation(chartFileName) {
-  if (settings.useDataUri) {
-    return makeWorkingFilePath(chartFileName + ".svg");
-  } else {
-    return makeOutputFilePath(chartFileName + ".svg");
-  }
-}
-
-function prepareChartFile(chartDefinition, chartFileName) {
-  if (!fs.existsSync(getChartFileLocation(chartFileName))) {
-    fs.writeFileSync(`${settings.workingFolder}/chart.mmd`, chartDefinition);
-    execSync(
-      `npx -p @mermaid-js/mermaid-cli mmdc -q --backgroundColor ${settings.backgroundColor} --cssFile ${makeWorkingFilePath("theme.css")} --input ${makeWorkingFilePath("chart.mmd")} --output ${getChartFileLocation(chartFileName)} --configFile ${makeWorkingFilePath("mermaidConfig.json")} --puppeteerConfigFile ${makeWorkingFilePath("puppeteerConfig.json")}`,
-      {
-        cwd: "./",
-        encoding: "utf-8",
-        stdio: "inherit",
-      },
-    );
-  }
+function prepareChartFile(chartDefinition) {
+  fs.writeFileSync(makeWorkingFilePath("chart.mmd"), chartDefinition);
+  execSync(
+    `npx -p @mermaid-js/mermaid-cli mmdc -q --backgroundColor ${settings.backgroundColor} --cssFile ${makeWorkingFilePath("theme.css")} --input ${makeWorkingFilePath("chart.mmd")} --output ${makeWorkingFilePath("chart.svg")} --configFile ${makeWorkingFilePath("mermaidConfig.json")} --puppeteerConfigFile ${makeWorkingFilePath("puppeteerConfig.json")}`,
+    {
+      cwd: "./",
+      encoding: "utf-8",
+      stdio: "inherit",
+    },
+  );
 }
 
 function mermaidChart(chartDefinition) {
-  const chartFileName = hashSum(chartDefinition);
-
   try {
-    prepareChartFile(chartDefinition, chartFileName);
-
+    prepareChartFile(chartDefinition);
     const chartTitle = extractChartTitle(chartDefinition);
+    const chart = fs
+      .readFileSync(makeWorkingFilePath("chart.svg"), "binary")
+      .toString();
 
-    if (settings.useDataUri) {
-      const chart = fs.readFileSync(
-        getChartFileLocation(chartFileName),
-        "binary",
-      );
-      const chartBase64 = Buffer.from(chart, "binary").toString("base64");
-      return `<figure class="mermaid"><img src=\"data:image/svg+xml;base64,${chartBase64}\"${settings.imageAttributes ? " " + settings.imageAttributes : ""}/>${chartTitle ? `<figcaption>${chartTitle}</figcaption>` : ""}</figure>`;
-    } else {
-      return `<figure class="mermaid"><img src=\"${makeUrlPath(chartFileName + ".svg")}\"${settings.imageAttributes ? " " + settings.imageAttributes : ""}/>${chartTitle ? `<figcaption>${chartTitle}</figcaption>` : ""}</figure>`;
-    }
+    return `<figure class="mermaid">${chart}${chartTitle ? `<figcaption>${chartTitle}</figcaption>` : ""}</figure>`;
   } catch (err) {
     console.error(
       chalk.red(`Failure rendering mermaid chart ${chartDefinition}`),
+      err,
     );
     return `<pre>${chartDefinition}</pre>`;
   }
--- test/index.mjs
+++ test/index.mjs
@@ -3,16 +3,12 @@ import markdownItMermaid from "../index.mjs";
 const md = markdownit();
 
 const TEST = `\`\`\`mermaid
-  sequenceDiagram
-      title "My sequence diagram"
-      participant Alice
-      participant Bob
-      Bob->>Alice: Hi Alice
-      Alice->>Bob: Hi Bob
-
+  flowchart LR
+    A(["Start"]) --> B{"Decision"}
+    B --> C["Option A"] & D["Option B"]
 \`\`\``;
 
-md.use(markdownItMermaid, { useDataUri: false });
+md.use(markdownItMermaid);
 
 const result = md.render(TEST);
 console.log(result);
